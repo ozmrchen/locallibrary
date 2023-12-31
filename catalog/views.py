3659ce4130c0d7a0b1b00590a990011ea039b0dc
jeremy886 <jeremy886@gmail.com>
2023-12-31 16:40:23 +1100

    Part 7 Sessions

--- catalog/views.py
+++ catalog/views.py
@@ -13,6 +13,10 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
@@ -21,6 +25,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
