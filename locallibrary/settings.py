d047c7259e654423e89e503c08e3b386d7a21f5b
ozmrchen <119475897+ozmrchen@users.noreply.github.com>
2024-01-02 19:48:37 +1100

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-+6_k%b7r0$kpx!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['locallibrary24.up.railway.app', '127.0.0.1']
 
 # Application definition
 
@@ -145,3 +145,5 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+CSRF_TRUSTED_ORIGINS = ['locallibrary24.up.railway.app']
